19941a482cbb0140c7de4f63aa123e019488c75d
Samuel Lopez <samuellopez01@hotmail.com>
2025-08-14 19:53:26 -0500

    Se crea navbar y se conecta con las contactenos y acerca de nosotros

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-// Botón de conoce más - página de about us 
+fetch("navbar.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("mtbNavbar").innerHTML = data;
+  });
+
+// Botón de conoce más - página de about us
 function mostrarDiv() {
   const div = document.getElementById("texto-oculto-mision-vision");
   if (div.style.display === "none" || div.style.display === "") {
@@ -6,4 +12,9 @@ function mostrarDiv() {
   } else {
     div.style.display = "none";
   }
+}
+
+// Función menú Hambuerguesa
+function mostrarMenu() {
+  document.getElementById("hamburguesa").classList.toggle("active");
 }
